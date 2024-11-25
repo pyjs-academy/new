2c09dab0855bd9dbfc06b13961d2e5570e24f327
pyjs-academy <pyjsacademy@gmail.com>
2024-11-25 21:55:33 +0500

    Update database.py

--- database.py
+++ database.py
@@ -1,5 +1,19 @@
 import sqlite3
 
+def create_db():
+    # Создание базы данных и таблицы пользователей, если она не существует
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL,
+        password TEXT NOT NULL
+    )
+    ''')
+    conn.commit()
+    conn.close()
+
 def get_user(username):
     # Соединение с базой данных
     conn = sqlite3.connect('users.db')
