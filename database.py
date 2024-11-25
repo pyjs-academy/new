c94473dfd91e28fde783ed8aa396fcad52dc95ec
pyjs-academy <pyjsacademy@gmail.com>
2024-11-25 22:11:32 +0500

    Update database.py

--- database.py
+++ database.py
@@ -1,8 +1,11 @@
-import sqlite3
+import os
+
+def get_db_path():
+    return os.path.join(os.getcwd(), 'users.db')
 
 def create_db():
-    # Создание базы данных и таблицы пользователей, если она не существует
-    conn = sqlite3.connect('users.db')
+    db_path = get_db_path()
+    conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS users (
@@ -15,25 +18,20 @@ def create_db():
     conn.close()
 
 def get_user(username):
-    # Соединение с базой данных
-    conn = sqlite3.connect('users.db')
+    db_path = get_db_path()
+    conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
-    
-    # Запрос пользователя по имени
     cursor.execute("SELECT * FROM users WHERE username = ?", (username,))
     user = cursor.fetchone()
     conn.close()
-    
     if user:
         return {"id": user[0], "username": user[1], "password": user[2]}
     return None
 
 def add_user(username, password):
-    # Соединение с базой данных
-    conn = sqlite3.connect('users.db')
+    db_path = get_db_path()
+    conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
-    
-    # Добавление пользователя в базу данных
     cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
     conn.commit()
     conn.close()
